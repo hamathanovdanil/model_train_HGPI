0aa6be3b4c4c4ddc559ae68349b78a60a1515cff
hamathanovdanil <149265329+hamathanovdanil@users.noreply.github.com>
2023-10-28 21:20:10 +0300

    Add files via upload

---
+++ model_train.py
@@ -0,0 +1,26 @@
+import spacy
+import pandas as pd
+from spacy.training import Example
+
+data = pd.read_csv("data_train.csv")
+print("Start model training")
+print("0/10569")
+nlp = spacy.load("ru_core_news_lg")
+nlp.remove_pipe("ner")
+nlp.add_pipe("ner")
+nlp.initialize()
+c=1
+examples = []
+for row in data.iterrows():
+    word = row[1]["word"]
+    labels = row[1]["label"]
+    predicted = nlp(word)
+    dict={"entities": ["U-"+labels]}
+    example = Example.from_dict(predicted, dict)
+    examples.append(example)
+    print(str(c) + "/10569")
+    c+=1
+print("Model training finished")
+nlp.begin_training()
+nlp.update(examples)
+nlp.to_disk("my_model")
