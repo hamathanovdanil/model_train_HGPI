040896f024b3cf2dfe54693ae2e476f2e5afc154
hamathanovdanil <149265329+hamathanovdanil@users.noreply.github.com>
2023-10-28 21:59:18 +0300

    Add files via upload

--- model_train.py
+++ model_train.py
@@ -5,6 +5,7 @@ from spacy.training import Example
 data = pd.read_csv("data_train.csv")
 print("Start model training")
 print("0/10569")
+spacy.cli.download("ru_core_news_lg")
 nlp = spacy.load("ru_core_news_lg")
 nlp.remove_pipe("ner")
 nlp.add_pipe("ner")
